ea9b546dd9c7636cf99214bf06f1efcfbd4cc309
Wuzi <77724001+DrWuzi@users.noreply.github.com>
2026-05-13 09:12:25 +0200

    integrate jQuery DataTables via CDN

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -3,5 +3,14 @@
 
     <!-- the support button on the top right -->
     <a class="support-button" href="https://affiliates.a2hosting.com/idevaffiliate.php?id=4471&url=579" target="_blank"></a>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#users-table').DataTable();
+            $('#admin-user-table').DataTable();
+        });
+    </script>
 </body>
 </html>

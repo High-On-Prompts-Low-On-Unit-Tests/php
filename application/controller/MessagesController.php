d5d8c9c8a107d9b40b9b59f3a4081b6c4c94459c
Wuzi <77724001+DrWuzi@users.noreply.github.com>
2026-05-27 07:19:40 +0200

    adding message controller

---
+++ application/controller/MessagesController.php
@@ -0,0 +1,38 @@
+<?php
+class MessagesController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        Auth::checkAuthentication();
+    }
+    public function index()
+    {
+        $me = Session::get('user_id');
+
+        $this->View->users = MessageModel::getChatPartners($me);
+
+        $partner_id = Request::get('partner');
+
+        if ($partner_id) {
+            MessageModel::markAsRead($partner_id, $me);
+            $this->View->active_partner_id = $partner_id;
+            $this->View->messages = MessageModel::getConversation($me, $partner_id);
+        } else {
+            $this->View->active_partner_id = null;
+            $this->View->messages = [];
+        }
+        $this->View->render('messages/index');
+    }
+    public function send()
+    {
+        $from = Session::get('user_id');
+        $to = Request::get('to');
+        $text = Request::get('text');
+
+        if (!empty($to) && strlen(trim($text)) > 0) {
+            MessageModel::sendMessage($from, $to, $text);
+        }
+        Redirect::to('messages/index?partner=' . $to);
+    }
+}
